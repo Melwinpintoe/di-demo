451c3408decf94fcb9fe3b3c7a583c0ad8edf725
Melwin Pinto <melwinpinto.tri@gmail.com>
2018-11-22 16:26:36 +0530

    from melwin - after @Profile and Default

--- src/main/java/guru/springframework/didemo/services/PrimaryGermanGreetingService.java
+++ src/main/java/guru/springframework/didemo/services/PrimaryGermanGreetingService.java
@@ -5,12 +5,11 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
-@Profile("en")
+@Profile({"de","default"})
 @Primary
-public class PrimaryGreetingService implements GreetingService {
-
+public class PrimaryGermanGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
-        return "Hello !! from primary greeting service";
+        return "Primarer Grubdienst";
     }
 }
