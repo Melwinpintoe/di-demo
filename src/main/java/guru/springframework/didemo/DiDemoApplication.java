e83de68debdeda434c5d233e9029fec2346980c0
Melwin Pinto <melwinpinto.tri@gmail.com>
2018-11-22 13:04:37 +0530

    from melwin - after @Primary

--- src/main/java/guru/springframework/didemo/DiDemoApplication.java
+++ src/main/java/guru/springframework/didemo/DiDemoApplication.java
@@ -1,6 +1,9 @@
 package guru.springframework.didemo;
 
-import guru.springframework.didemo.controller.MyController;
+import guru.springframework.didemo.controllers.ConstructorInjectedController;
+import guru.springframework.didemo.controllers.GetterInjectedController;
+import guru.springframework.didemo.controllers.MyController;
+import guru.springframework.didemo.controllers.PropertyInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -11,7 +14,13 @@ public class DiDemoApplication {
     public static void main(String[] args)
     {
        ApplicationContext ctx =  SpringApplication.run(DiDemoApplication.class, args);
-       MyController  myController = (MyController) ctx.getBean("myController");
-       myController.hello();
+        MyController myController = (MyController) ctx.getBean("myController");
+       // myController.hello();
+
+        System.out.println(myController.hello());
+        System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
+        System.out.println(ctx.getBean(GetterInjectedController.class).sayHello());
+        System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+
     }
 }
